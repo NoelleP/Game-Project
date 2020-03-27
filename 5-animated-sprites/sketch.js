e159e815a7781492ca4af6636b03c622b0dacaed
NoelleP <61802909+NoelleP@users.noreply.github.com>
2020-03-27 13:44:27 -0700

    Bird Drawn and Animated

--- 5-animated-sprites/sketch.js
+++ 5-animated-sprites/sketch.js
@@ -2,7 +2,7 @@
 //
 // animations like p5 images should be stored in variables
 // in order to be displayed during the draw cycle
-var ghost;
+var bird;
 //
 // //it's advisable (but not necessary) to load the images in the preload function
 // //of your sketch otherwise they may appear with a little delay
@@ -10,19 +10,19 @@ function preload() {
 //
 //   //create an animation from a sequence of numbered images
 //   //pass the first and the last file name and it will try to find the ones in between
-  ghost = loadAnimation('assets/ghost_standing0001.png', 'assets/ghost_standing0007.png');
+  bird = loadAnimation('sprites/Bird001.png', 'sprites/Bird002.png', 'sprites/Bird003.png', 'sprites/Bird004.png', 'sprites/Bird005.png');
 
 }
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(1000, 750);
 }
 
 function draw() {
-  background(200, 255, 255);
+  background(255);
 
   //specify the animation instance and its x,y position
   //animation() will update the animation frame as well
-  animation(ghost, 300, 150);
+  animation(bird, 500, 500);
 
 }
